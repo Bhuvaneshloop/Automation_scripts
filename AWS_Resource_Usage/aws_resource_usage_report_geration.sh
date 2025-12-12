f088a40e3905486984a375689761c6eeac00e323
bhuvanesh <bhuvanesh02003@gmail.com>
2025-12-12 06:44:44 +0000

    new automation of aws resource monitoring script

---
+++ AWS_Resource_Usage/aws_resource_usage_report_geration.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#############################################
+#Author:Bhuvanesh(Support Team)
+#Date created:12/12/2025
+
+#AWS Resource Usage Monitoring Report
+#############################################
+
+#Resources
+#S3
+#IAM
+#EC2
+#LAMBDA FUNCTION
+#
+set -x
+set -e
+set -o pipefail
+
+mkdir -p Logs
+log="Logs/log-$(date +%F-%H-%M-%S).log"
+echo "##### AWS Resource Usage Monitoring Report ######" >> "$log"
+
+echo -e "\n1)List of IAM Users" >> "$log"
+aws iam list-users | jq -r '.Users[].UserName' >> "$log"
+
+echo -e "\n2)Running EC2 instances" >> "$log"
+aws ec2 describe-instances | jq -r '.Reservations[].Instances[].InstanceId' >> "$log"
+
+echo -e "\n3)List of S3 in Use" >> "$log"
+aws s3 ls >> "$log"
+
+echo "Report saved to the LOG"
