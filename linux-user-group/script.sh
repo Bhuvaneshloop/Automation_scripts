0278b343902a3641309e194f1e3a4412f566b09c
bhuvanesh <bhuvanesh02003@gmail.com>
2025-12-16 09:33:05 +0000

    Convert linux-user-group to normal directory

---
+++ linux-user-group/script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#######################################
+#Linux User Mnagement Script
+#######################################
+
+
+if [[ $EUID -ne 0 ]]; then
+   echo "the user is not root"
+   exit 1
+fi
+
+read -p "enter the username : " user
+read -p "enter the groupname : " group
+
+
+if [[ -z $user || -z $group ]]; then
+   echo "the user name or group name  cant be empty"
+   exit 1
+fi
+
+if getent group $group > /dev/null; then
+	echo "$group already exist"
+else 
+	groupadd "$group" 
+	echo "created the group $group sucessfully"
+fi
+
+if id $user &>/dev/null; then
+	echo "$user exist already"
+else 
+	useradd -m -g "$group" "$user"
+	echo "$user added to the group $group sucessfully"
+fi
+
