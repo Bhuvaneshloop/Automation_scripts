08f72140c62e456a314765f884a051e3fd9f7b83
bhuvanesh <bhuvanesh02003@gmail.com>
2026-05-18 15:37:31 +0530

    updated some more logics like , removed repetative logic in case with directly updating checking deployment dir

--- WebServerHealth/Scripts/deploy.sh
+++ WebServerHealth/Scripts/deploy.sh
@@ -1,55 +1,62 @@
 #!/bin/bash
-set -a
-source app.env
-set +a
+
+if [ -f app.env ];then
+        source app.env
+        echo "APP.ENV FILE AVAIABLE"
+else
+        echo "APP.ENV IS NOT AVALAIBE"
+        exit 1
+fi
+
+BACKUP="$BACKUP_FOLDER/$(date +%F_%H-%M-%S)"
+
+
 info1(){
 	echo "This file will helps us to"
 }
+
 
 while getopts ":i" opt;
 do
 	case $opt in 
 		i)
-                     info1 ;;
+                     info1
+                     exit 1 ;;
                 :)
 	             echo "Option needed , invalid or empty option are not allowed"
 		     ;;
      esac
-     shift
 done
- 
-if [ -f app.env ];then
-	echo "APP.ENV FILE AVAIABLE"
-else
-	echo "APP.ENV IS NOT AVALAIBE"
-	exit 1
-fi
+
+shift $((OPTIND - 1))
 
 Backup(){
-	cp -r $CURRENT_RUNNING $BACKUP_FOLDER/$(date +%F_%H:%M:%S)
-        echo "Backup Sucessfull"	
+	cp -r "$CURRENT_RUNNING" "$BACKUP"
+        rm -rf "$CURRENT_RUNNING"/*
+        echo "Backup Sucessfull"
+}
+
+rollback(){
+  rm -rf "$CURRENT_RUNNING"/*
+  cp -r "$BACKUP"/* "$CURRENT_RUNNING"/
 }
 
 Deply_Latest(){
-        cp -r $VERSIONS/$1/* $CURRENT_RUNNING/
+        cp -r "$VERSIONS"/"$1"/* "$CURRENT_RUNNING"/
 	echo "Deployment Sucessfull of latest version $1"
 }
-case $1 in 
-	V1)
 
+if [ -d  /home/bhuvaneshp3/awstutor/WebServerHealth/Deployment/"$1" ];then
 	  Backup  
 	  Deply_Latest $1
-	  ;;
-	V2)
-	  Backup
-	  Deply_Latest $1 
-	  ;;
-	*)
-	  echo "Version not Available either empty"
-	  ;;
-esac 
-
-
-
+          
+          if curl -s http://localhost:80  >/dev/null;then
+              echo "Deployment Sucessfull of latest version $1"
+         else
+              rollback
+          fi
+else
+ echo "the mentioned version is not available"
+fi
 
 
